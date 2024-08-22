b081ba1a1c9920fcf8e99ad090dcc4ad00749fde
CJNickson <cn125047@student.wpsok.org>
2024-08-22 13:38:49 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,11 @@
+#name CJ Nickson
+#class 6th hour
+#assigment HW2
+print("helloworld")
+
+input("what is your name")
+
+x=int(input('give me 1 integer'))
+y=int(input('give me another integer'))
+z=x+y
+print(z)
