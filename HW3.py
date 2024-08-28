9f73928382663a3f1851e674e4995c9936d851cd
CJNickson <cn125047@student.wpsok.org>
2024-08-28 13:59:06 -0500

    added HW3

---
+++ HW3.py
@@ -0,0 +1,21 @@
+#name CJ Nickson
+#class 6th hour
+#assigment HW3
+print("helloworld")
+
+classlist = ["cj", "evan","elijah",'seeley',"aj"]
+
+classlist.append(input())
+
+print(f"{classlist[3]} is HERE!")
+
+numlist =["1","35","88","23"]
+
+numlist.sort()
+print(numlist)
+
+numlist.sort()
+print(numlist)
+
+sum = numlist[0] + numlist[1] +numlist[2]
+print(sum)
