b3f6939ff4b9a82aebe2598b593ab6a1622484d3
CJNickson <cn125047@student.wpsok.org>
2024-09-27 13:59:41 -0500

    scenario 2

---
+++ HW5.py
@@ -0,0 +1,14 @@
+#name CJ Nickson
+#class 6th hour
+#assigment HW5
+
+print("helloworld")
+
+numlist = [1,2,3]
+
+if numlist[0]> numlist[1] and numlist[0]> numlist[2]:
+    print("1 is thw greatest")
+elif numlist[1]> numlist[0] and numlist[1]> numlist[2]:
+    print(" 2 is the greatest")
+elif numlist[2]> numlist[1] and numlist[2]> numlist[0]:
+    print("3 is the greatest")
