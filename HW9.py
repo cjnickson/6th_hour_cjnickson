08b92852e46b4743dfca33343b01707e31bfa559
CJNickson <cn125047@student.wpsok.org>
2024-10-24 13:46:54 -0500

    HW9

---
+++ HW9.py
@@ -0,0 +1,17 @@
+#Name: CJ Nickson
+#Class: 6th Hour
+#Assignment: HW9
+
+#1. Print Hello World!
+print("Hello world!")
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+temp=20
+if temp>20:
+    print("it's hot")
+elif temp>10:
+     print("it's mild")
+else :
+     print("it's cold")
+
+print("thank you!")
