b312a0670ef49d7a676dd797543d8b666d294227
CJNickson <cn125047@student.wpsok.org>
2024-10-30 13:50:46 -0500

    HW10

---
+++ HW10.py
@@ -0,0 +1,24 @@
+#Name: CJ Nickson
+#Class: 6th Hour
+#Assignment: HW10
+
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the end.
+i=5
+#ehile i >= 1:
+# print(i)
+# time.sleep(1)
+#else:
+# print ( "hello world")
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+j =2
+#AHAIL J <= 2==0:
+# print(j)
+# j += 2
+#3. Create a while loop that repeats until the user
+#inputs the number 0.
+k = int(input())
+while k != 1:
+    print(k)
+    k = int(input())
